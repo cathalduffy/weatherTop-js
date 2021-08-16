bb2ba422f96cfdf4dcb7400dcae320297bede9cf
Glitch (big-bitter-point) <none>
2021-08-16 15:27:56 +0000

    👬🍚 Checkpoint ./models/playlist-store.json:32830354/1163 ./models/station-store.js:32830354/785

---
+++ models/station-store.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const _ = require("lodash");
+const JsonStore = require("./json-store");
+
+const stationStore = {
+  store: new JsonStore("./models/station-store.json", {
+    stationCollection: []
+  }),
+  collection: "stationCollection",
+
+  getAllStations() {
+    return this.store.findAll(this.collection);
+  },
+
+  getStation(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+
+  getUserStations(userid) {
+    return this.store.findBy(this.collection, { userid: userid });
+  },
+
+  addStation(station) {
+    this.store.add(this.collection, station);
+    this.store.save();
+  },
+
+  removeStation(id) {
+    const station = this.getStation(id);
+    this.store.remove(this.collection, station);
+    this.store.save();
+  },
+
+  removeAllStations() {
+    this.store.removeAll(this.collection);
+    this.store.save();
+  },
+
+  addReading(id, reading) {
+    const station = this.getStation(id);
+    station.readings.push(reading);
+
+    let pressure = 0;
+    for (let i = 0; i < station.readings.length; i++) {
+      pressure += station.readings[i].pressure;
+    }
+
+    station.pressure = pressure;
+    this.store.save();
+  },
+
+  removeReading(id, readingId) {
+    const station = this.getStation(id);
+    const readings = station.readings;
+    _.remove(readings, { id: readingId });
+    this.store.save();
+  },
+
+  getReading(id, readingId) {
+    const station = this.store.findOneBy(this.collection, { id: id });
+    const songs = station.readings.filter(song => song.id == songId);
+    return songs[0];
+  },
+
+  updateSong(song, updatedSong) {
+    song.title = updatedSong.title;
+    song.artist = updatedSong.artist;
+    song.duration = updatedSong.duration;
+    this.store.save();
+  }
+};
+
+module.exports = playlistStore;
