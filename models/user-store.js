bf349a9d4b14eab93c0dd83e70249dbfd4e3aab1
Glitch (big-bitter-point) <none>
2021-08-19 14:26:28 +0000

    🏄🍯 Checkpoint ./models/user-store.js:32830354/137

--- models/user-store.js
+++ models/user-store.js
@@ -22,7 +22,11 @@ const userStore = {
 
   getUserByEmail(email) {
     return this.store.findOneBy(this.collection, { email: email });
-  }
-};
+  },
+     
+getUserByPassword(password) {
+    return this.store.findOneBy(this.collection, { password: password });
+  },
+}
 
 module.exports = userStore;
