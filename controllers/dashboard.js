dc9d58e1c573af771ad2e96a8615d6a17aa6fc12
Glitch (big-bitter-point) <none>
2021-08-23 22:33:03 +0000

    🍏🐫 Checkpoint ./controllers/dashboard.js:32830354/2336 ./views/partials/liststations.hbs:32830354/344

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -12,34 +12,13 @@ const dashboard = {
     const loggedInUser = accounts.getCurrentUser(request);
     const stationId = request.params.id;
     logger.debug("Station id = ", stationId);
-    
-  for (Station station : stations) {
-    const station = stationStore.getStation(stationId);
-    const latestReading = stationAnalytics.getLatestReading(station);
-    console.log(latestReading);
-    const minTemp = stationAnalytics.getMinTemp(station);
-    const maxTemp = stationAnalytics.getMaxTemp(station);
-    const minWindSpeed = stationAnalytics.getMinWindSpeed(station);
-    const maxWindSpeed = stationAnalytics.getMaxWindSpeed(station);
-    const minPressure = stationAnalytics.getMinPressure(station);
-    const maxPressure = stationAnalytics.getMaxPressure(station);
-    
+
     const viewData = {
       title: "Station Dashboard",
       stations: stationStore.getUserStations(loggedInUser.id),
-      station: stationStore.getStation(stationId),
-      latestReading: latestReading,
-      maxTemp: maxTemp,
-      minTemp: minTemp,
-      maxWindSpeed: maxWindSpeed,
-      minWindSpeed: minWindSpeed,
-      maxPressure: maxPressure,
-      minPressure: minPressure,
-    
     };
     logger.info("about to render", stationStore.getAllStations());
-    response.render("dashboard", viewData, stations);
-  }
+    response.render("dashboard", viewData);
   },
 
   deleteStation(request, response) {
