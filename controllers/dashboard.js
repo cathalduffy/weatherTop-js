aa6c73e07f4b4cd17ec72dfbbdf5b11373eb5d86
Glitch (big-bitter-point) <none>
2021-08-23 20:37:07 +0000

    👝👋 Checkpoint ./controllers/dashboard.js:32830354/797

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,14 +4,32 @@ const accounts = require("./accounts.js");
 const logger = require("../utils/logger");
 const stationStore = require("../models/station-store");
 const uuid = require("uuid");
+const stationAnalytics = require('../utils/station-analytics');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const loggedInUser = accounts.getCurrentUser(request);
+    
+    const station = stationStore.getStation(stationId);
+    const latestReading = stationAnalytics.getLatestReading(station);
+    console.log(latestReading);
+    const minTemp = stationAnalytics.getMinTemp(station);
+    const maxTemp = stationAnalytics.getMaxTemp(station);
+    const minWindSpeed = stationAnalytics.getMinWindSpeed(station);
+    const maxWindSpeed = stationAnalytics.getMaxWindSpeed(station);
+    const minPressure = stationAnalytics.getMinPressure(station);
+    const maxPressure = stationAnalytics.getMaxPressure(station);
+    
     const viewData = {
       title: "Station Dashboard",
-      stations: stationStore.getUserStations(loggedInUser.id)
+      stations: stationStore.getUserStations(loggedInUser.id),
+      maxTemp: maxTemp,
+      minTemp: minTemp,
+      maxWindSpeed: maxWindSpeed,
+      minWindSpeed: minWindSpeed,
+      maxPressure: maxPressure,
+      minPressure: minPressure,
     };
     logger.info("about to render", stationStore.getAllStations());
     response.render("dashboard", viewData);
