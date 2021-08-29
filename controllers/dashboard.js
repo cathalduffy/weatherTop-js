42c39e14a46785df524a8215dfcd399d4257b9b8
Glitch (big-bitter-point) <none>
2021-08-29 17:16:58 +0000

    🚣🚙 Checkpoint ./utils/station-analytics.js:32830354/64 ./controllers/dashboard.js:32830354/402 ./views/dashboard.hbs:32830354/31

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -14,10 +14,14 @@ const dashboard = {
     const stationId = request.params.id;
     logger.debug("Station id = ", stationId);
     
+    const station = stationStore.getStation(stationId);
+    
     const stations = stationStore.getUserStations(loggedInUser.id);
       for (let station in stations) {
-        station.maxTemp = stationAnalytics.getMaxTemp();
+        const maxTemp = stationAnalytics.getMaxTemp(station);
     }
+    
+    const maxTemp = stationAnalytics.getMaxTemp(station);
     
    
     const viewData = {
--- utils/station-analytics.js
+++ utils/station-analytics.js
@@ -23,6 +23,8 @@ const stationAnalytics = {
   
   
   
+  
+  
 
 
  
